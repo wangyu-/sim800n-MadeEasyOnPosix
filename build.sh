1bd2a0a0b0e7cf3b5d3a1a6f781218a6169aabf1
yancey <yancey@whist.com>
2025-01-24 22:34:53 -0500

    add cmake; add depend files

---
+++ build.sh
@@ -0,0 +1,1 @@
+g++ -I. -Iverilator/include -Iobj_dir -I/opt/homebrew/include/SDL2 -D_THREAD_SAFE *.cpp obj_dir/*.cpp verilator/include/verilated.cpp  -L/opt/homebrew/lib -lSDL2 -Wl,-framework,Cocoa -std=c++11 -O2
